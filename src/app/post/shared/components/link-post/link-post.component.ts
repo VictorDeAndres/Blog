b4c270e895ea11e6b10dd134b5fc0b9177ce7885
Victor <victor@iMac-de-Capitan.local>
2019-05-05 12:03:59 +0200

    [Add] Add links to other posts

---
+++ src/app/post/shared/components/link-post/link-post.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'blog-link-post',
+  templateUrl: './link-post.component.html',
+  styleUrls: ['./link-post.component.scss']
+})
+export class LinkPostComponent implements OnInit {
+
+  @Input() sourcePhoto: string;
+  @Input() postTitle: string;
+  @Input() postSubTitle: string;
+  @Input() postPublishInfo: string;
+  @Input() postLink: string;
+  @Input() idLink: string;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      if ( this.sourcePhoto ) {
+        document.querySelector(`.link__post__photo#${this.idLink}`)['src'] = this.sourcePhoto;
+      }
+      if ( this.postTitle ) {
+        document.querySelector(`.link__post__title__title#${this.idLink}`).innerHTML = this.postTitle;
+      }
+      if ( this.postSubTitle ) {
+        document.querySelector(`.link__post__title__subTitle#${this.idLink}`).innerHTML = this.postSubTitle;
+      }
+      if ( this.postPublishInfo ) {
+        document.querySelector(`.link__post__title__publishInfo#${this.idLink}`).innerHTML = this.postPublishInfo;
+      }
+    }, 0);
+  }
+
+  public openLink(): void {
+    this.router.navigate([this.postLink]);
+  }
+
+}
